e16eeaf91685090bed7a00fa9ce392cae84a44e1
Tomas <darwindarwindarwin2000@gmail.com>
2023-02-12 19:44:11 -0300

    Deshabilite el login obligatorio para testear

--- app/src/main/java/app/lajusta/MainActivity.kt
+++ app/src/main/java/app/lajusta/MainActivity.kt
@@ -1,8 +1,10 @@
 package app.lajusta
 
+import android.content.ClipData
 import android.content.Intent
 import android.os.Bundle
 import android.view.Menu
+import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
 import androidx.drawerlayout.widget.DrawerLayout
 import androidx.navigation.findNavController
@@ -22,12 +24,18 @@ class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
 
     companion object {
-        const val baseUrl: String = "http://192.168.0.120:80/api/"
+        const val baseUrl: String = "http://192.168.0.15:80/api/"
     }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
+        //Setup
+
+        if ( (!intent.hasExtra("userType")) || (!intent.hasExtra("displayName"))) {
+            val i = Intent(applicationContext, LoginActivity::class.java)
+            startActivity(i)
+        }
         val displayName = intent.getStringExtra("displayName")
         val userType = intent.getStringExtra("userType")
 
@@ -37,8 +45,6 @@ class MainActivity : AppCompatActivity() {
         setSupportActionBar(binding.appBarMain.toolbar)
 
         binding.appBarMain.fab.setOnClickListener { view ->
-            //val i = Intent(applicationContext, LoginActivity::class.java)
-            //startActivity(i)
             //Voy a usar esto para comprobar si mandan los datos correctos del login:
             if (userType == UserType.ADMIN.toString()) {
                 Snackbar.make(view, "Tienes permiso de administrador", Snackbar.LENGTH_LONG)
@@ -77,5 +83,13 @@ class MainActivity : AppCompatActivity() {
     override fun onSupportNavigateUp(): Boolean {
         val navController = findNavController(R.id.nav_host_fragment_content_main)
         return navController.navigateUp(appBarConfiguration) || super.onSupportNavigateUp()
+    }
+
+    fun goToLogin() {
+        val i = Intent(applicationContext, LoginActivity::class.java)
+        startActivity(i)
+    }
+    interface onItemClickListener {
+        fun goToLogin()
     }
 }
