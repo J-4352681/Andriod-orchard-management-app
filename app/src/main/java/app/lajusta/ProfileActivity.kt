c24a1a19f0c112aa322794d3b29f6d8e2146c49f
Tomas <darwindarwindarwin2000@gmail.com>
2023-02-12 22:18:05 -0300

    Cambie el lugar donde esta el layout de profile y mejore el layout de profile.

--- app/src/main/java/app/lajusta/ProfileActivity.kt
+++ app/src/main/java/app/lajusta/ProfileActivity.kt
@@ -1,11 +1,35 @@
 package app.lajusta
 
+import android.annotation.SuppressLint
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.EditText
 
 class ProfileActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.menu.activity_profile)
+        setContentView(R.layout.activity_profile)
+
+        //Setup
+        val displayName = intent.getStringExtra("displayName")
+        val userType = intent.getStringExtra("userType")
+
+        //actionbar
+        val actionbar = supportActionBar
+        //set actionbar title
+        actionbar!!.title = "Perfil"
+        //set back button
+        actionbar.setDisplayHomeAsUpEnabled(true)
+        actionbar.setDisplayHomeAsUpEnabled(true)
+
+        //Username
+        val editText = findViewById<EditText>(R.id.editTextUsername)
+        editText.hint = displayName
+
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        onBackPressed()
+        return true
     }
 }
