b367d0ee340e13f934d74bbe949fe7461182ca78
Jeremias Salsamendi <jere4352681@gmail.com>
2023-04-01 13:30:16 -0300

    Corrijo muestra de verduras de listado de bolsones

--- app/src/main/java/app/lajusta/MainActivity.kt
+++ app/src/main/java/app/lajusta/MainActivity.kt
@@ -30,8 +30,8 @@ class MainActivity : AppCompatActivity() {
 
     companion object {
         //const val baseUrl: String = "http://163.10.141.61:80/api/" // LABO
-        const val baseUrl: String = "http://192.168.0.15:80/api/" // TOMI
-        //const val baseUrl: String = "http://192.168.0.120:80/api/" // JERE
+        //const val baseUrl: String = "http://192.168.0.15:80/api/" // TOMI
+        const val baseUrl: String = "http://192.168.0.120:80/api/" // JERE
         //const val baseUrl: String = "http://192.168.0.254:80/api/" // JERE
 
         const val LOGIN_DEBUG = true //Permite usuarios "Administrador Administrador", "Tecnico Tecnico" y "aaaaaa aaaaaa" para entrar por defecto a la app
